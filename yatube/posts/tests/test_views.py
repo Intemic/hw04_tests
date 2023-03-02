9875e8de2f3ad80fe1676618389cce882dfa73c4
Лучик Антон <Intemic@yandex.ru>
2023-03-02 19:18:33 +0500

    Устранение замечаний

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -9,6 +9,65 @@ from posts.models import Group, Post, User
 
 
 class TestView(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.user_pshk = User.objects.create_user(username='pshk')
+        cls.group1 = Group.objects.create(
+            title='Group1',
+            slug='group1',
+            description='Group1'
+        )
+        cls.post = Post.objects.create(
+            text='Пост 1',
+            author=cls.user_pshk,
+            group=cls.group1
+        )
+
+        cls.urls_pattern = (
+            (
+                reverse('posts:index'),
+                'posts/index.html',
+            ),
+
+            (
+                reverse(
+                    'posts:group_list',
+                    kwargs={'slug': cls.group1.slug}
+                ),
+                'posts/group_list.html',
+            ),
+
+            (
+                reverse(
+                    'posts:profile',
+                    kwargs={'username': cls.user_pshk.username}
+                ),
+                'posts/profile.html',
+            ),
+
+            (
+                reverse(
+                    'posts:post_detail',
+                    kwargs={'post_id': cls.post.pk}
+                ),
+                'posts/post_detail.html',
+            ),
+
+            (
+                reverse(
+                    'posts:post_edit',
+                    kwargs={'post_id': cls.post.pk}
+                ),
+                'posts/create_post.html',
+            ),
+
+            (
+                reverse('posts:post_create'),
+                'posts/create_post.html',
+            )
+        )
+
     def compare_posts(self, post: Post, post_ref: Post) -> None:
         self.assertIsInstance(post, Post)
         self.assertEqual(post.text, post_ref.text)
@@ -16,39 +75,25 @@ class TestView(TestCase):
         self.assertEqual(post.author, post_ref.author)
 
     def compare_groups(self, group: Group, group_ref: Group) -> None:
+        self.assertIsInstance(group, Group)
         self.assertEqual(group.title, group_ref.title)
         self.assertEqual(group.slug, group_ref.slug)
         self.assertEqual(group.description, group_ref.description)
 
     def setUp(self):
-        self.user_pshk = User.objects.create_user(username='pshk')
-
         self.author_client = Client()
         self.author_client.force_login(self.user_pshk)
 
-        self.group1 = Group.objects.create(
-            title='Group1',
-            slug='group1',
-            description='Group1'
-        )
-
-        self.post = Post.objects.create(
-            text='Пост 1',
-            author=self.user_pshk,
-            group=self.group1
-        )
-
-    def tearDown(self) -> None:
-        User.objects.all().delete()
-        Group.objects.all().delete()
-        Post.objects.all().delete()
+    def test_patterns(self):
+        for url, pattern in TestView.urls_pattern:
+            with self.subTest(url=url):
+                response = self.author_client.get(url)
+                self.assertTemplateUsed(response, pattern)
 
     def test_main_page(self):
         """Проверка главной страницы(контекст, шаблон, данные)."""
         url = reverse('posts:index')
         response = self.author_client.get(url)
-
-        self.assertTemplateUsed(response, 'posts/index.html')
 
         page = response.context.get('page_obj')
         self.assertIsInstance(page, Page)
@@ -56,31 +101,12 @@ class TestView(TestCase):
         self.compare_posts(page.object_list[0], self.post)
 
     def test_group_list_page(self):
-        """Проверка страницыe(контекст, шаблон, данные)."""
-        # добавим еще одну группу
-        group2 = Group.objects.create(
-            title='Group2',
-            slug='group2',
-            description='Group2'
-        )
-
-        url = reverse(
-            'posts:group_list',
-            kwargs={'slug': group2.slug}
-        )
-
-        # проверим что пост не попал на другую страницу
-        response = self.author_client.get(url)
-        page = response.context.get('page_obj')
-        self.assertNotIn(self.post, page.object_list)
-
-        # проверим обратное
+        """Проверка страницы(контекст, шаблон, данные)."""
         url = reverse(
             'posts:group_list',
             kwargs={'slug': self.post.group.slug}
         )
         response = self.author_client.get(url)
-        self.assertTemplateUsed(response, 'posts/group_list.html')
 
         page = response.context.get('page_obj')
         self.assertIsInstance(page, Page)
@@ -93,27 +119,12 @@ class TestView(TestCase):
 
     def test_profile_page(self):
         """Проверка профиля(контекст, шаблон, данные)."""
-        # содаем второго пользователя
-        user_leo = User.objects.create_user(username='leo')
-
-        url = reverse(
-            'posts:profile',
-            kwargs={'username': user_leo.username}
-        )
-
-        # проверим что пост не попал на другую страницу
-        response = self.author_client.get(url)
-        page = response.context.get('page_obj')
-        self.assertNotIn(self.post, page.object_list)
-
-        # проверим обратное
         url = reverse(
             'posts:profile',
             kwargs={'username': self.post.author.username}
         )
 
         response = self.author_client.get(url)
-        self.assertTemplateUsed(response, 'posts/profile.html')
 
         page = response.context.get('page_obj')
         self.assertIsInstance(page, Page)
@@ -126,24 +137,15 @@ class TestView(TestCase):
 
     def test_post_detail_page(self):
         """Проверка страницы поста(контекст, шаблон, данные)."""
-        post2 = Post.objects.create(
-            text='Пост 2',
-            author=self.user_pshk,
-            group=self.group1
-        )
-
         url = reverse(
             'posts:post_detail',
             kwargs={'post_id': self.post.pk}
         )
 
         response = self.author_client.get(url)
-        self.assertTemplateUsed(response, 'posts/post_detail.html')
 
         post = response.context.get('post')
         self.assertIsInstance(post, Post)
-        self.assertNotEqual(post.pk, post2.pk)
-
         self.compare_posts(post, self.post)
 
     def test_post_edit_page(self):
@@ -154,7 +156,6 @@ class TestView(TestCase):
         )
 
         response = self.author_client.get(url)
-        self.assertTemplateUsed(response, 'posts/create_post.html')
         self.assertIsInstance(response.context['form'], PostForm)
 
         form_fields = {
@@ -175,7 +176,6 @@ class TestView(TestCase):
         url = reverse('posts:post_create')
 
         response = self.author_client.get(url)
-        self.assertTemplateUsed(response, 'posts/create_post.html')
         self.assertIsInstance(response.context['form'], PostForm)
 
         form_fields = {
@@ -247,25 +247,25 @@ class TestView(TestCase):
         )
 
         Post.objects.all().delete()
+        # кол-во объектов на второй странице
+        number_obj_on_secon_page = round(settings.NUMBER_OF_LINES_ON_PAGE / 2)
+        count_objects = (settings.NUMBER_OF_LINES_ON_PAGE
+                         + number_obj_on_secon_page)
 
         page_count_post = (
             (1, settings.NUMBER_OF_LINES_ON_PAGE),
-            (2, round(settings.NUMBER_OF_LINES_ON_PAGE / 2))
+            (2, number_obj_on_secon_page)
         )
 
-        post_list = []
-        for page, count in page_count_post:
-            while count:
-                count -= 1
-                post_list.append(
-                    Post(
-                        text='Пост № ' + str(len(post_list) + 1),
-                        author=self.user_pshk,
-                        group=self.group1
-                    )
-                )
-
-        Post.objects.bulk_create(post_list)
+        Post.objects.bulk_create(
+            [
+                Post(
+                    text='Пост № ' + str(i + 1),
+                    author=self.user_pshk,
+                    group=self.group1
+                ) for i in range(count_objects)
+            ]
+        )
 
         for url in urls:
             for page_n, count_post_in_page in page_count_post:
@@ -277,3 +277,64 @@ class TestView(TestCase):
                         len(response.context['page_obj']),
                         count_post_in_page
                     )
+
+    def test_for_not_posting_in_another_group(self):
+        """Проверим что при наличии нескольких групп.
+
+        пост не отображается в не свонй группе
+        """
+        # добавим еще одну группу
+        group2 = Group.objects.create(
+            title='Group2',
+            slug='group2',
+            description='Group2'
+        )
+
+        url = reverse(
+            'posts:group_list',
+            kwargs={'slug': group2.slug}
+        )
+
+        # проверим что пост не попал на другую страницу
+        response = self.author_client.get(url)
+        page = response.context.get('page_obj')
+        self.assertNotIn(self.post, page.object_list)
+
+    def test_for_not_posting_in_another_user(self):
+        """Проверим что при наличии нескольких пользователей.
+
+        пост не отображается у другого user'a
+        """
+        # содаем второго пользователя
+        user_leo = User.objects.create_user(username='leo')
+
+        url = reverse(
+            'posts:profile',
+            kwargs={'username': user_leo.username}
+        )
+
+        # проверим что пост не попал на другую страницу
+        response = self.author_client.get(url)
+        page = response.context.get('page_obj')
+        self.assertNotIn(self.post, page.object_list)
+
+    def test_for_the_absence_of_another_post_on_detail_page(self):
+        """Проверим что при наличии нескольких постов.
+
+        не отображается другой
+        """
+        post2 = Post.objects.create(
+            text='Пост 2',
+            author=self.user_pshk,
+            group=self.group1
+        )
+
+        url = reverse(
+            'posts:post_detail',
+            kwargs={'post_id': self.post.pk}
+        )
+
+        response = self.author_client.get(url)
+
+        post = response.context.get('post')
+        self.assertNotEqual(post.pk, post2.pk)
